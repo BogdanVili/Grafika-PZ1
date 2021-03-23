3c72d3c41b1d17cf3de47b291c501fd38340800b
Bogdan Vilimonovic <bogdan.vilimonovic@gmail.com>
2021-03-23 22:01:06 +0100

    ellipse and rectangle

--- Grafika PZ1/Grafika PZ1/MainWindow.xaml.cs
+++ Grafika PZ1/Grafika PZ1/MainWindow.xaml.cs
@@ -20,30 +20,48 @@ namespace Grafika_PZ1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string shapeSelection;
+
+        public string ShapeSelection
+        {
+            get { return shapeSelection; }
+            set { shapeSelection = value; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            shapeSelection = "";
             this.DataContext = this;
         }
 
         private void EllipseButton_Click(object sender, RoutedEventArgs e)
         {
-
+            shapeSelection = "Ellipse";
         }
 
         private void RectangleButton_Click(object sender, RoutedEventArgs e)
         {
-
+            shapeSelection = "Rectangle";
         }
 
         private void PolygonButton_Click(object sender, RoutedEventArgs e)
         {
-
+            shapeSelection = "Polygon";
         }
 
         private void ImageButton_Click(object sender, RoutedEventArgs e)
         {
+            shapeSelection = "Image";
+        }
 
+        private void PaintingCanvas_MouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (shapeSelection == "Ellipse" || shapeSelection == "Rectangle")
+            {
+                ShapesSettings shapesSettings = new ShapesSettings(shapeSelection, PaintingCanvas, Mouse.GetPosition(PaintingCanvas));
+                shapesSettings.ShowDialog();
+            }
         }
     }
 }
---
+++ Grafika PZ1/Grafika PZ1/ShapesSettings.xaml.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Grafika_PZ1
+{
+    /// <summary>
+    /// Interaction logic for ShapesSettings.xaml
+    /// </summary>
+    public partial class ShapesSettings : Window
+    {
+        private string shapeSelection;
+
+        public string ShapeSelection
+        {
+            get { return shapeSelection; }
+            set { shapeSelection = value; }
+        }
+
+        private Canvas paintingCanvas;
+
+        public Canvas PaintingCanvas
+        {
+            get { return paintingCanvas; }
+            set { paintingCanvas = value; }
+        }
+
+        private Point canvasLocation;
+
+        public Point CanvasLocation
+        {
+            get { return canvasLocation; }
+            set { canvasLocation = value; }
+        }
+
+
+        public ShapesSettings(string shapeSelection, Canvas paintingCanvas, Point canvasLocation)
+        {
+            InitializeComponent();
+            this.shapeSelection = shapeSelection;
+            this.paintingCanvas = paintingCanvas;
+            this.canvasLocation = canvasLocation;
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DrawButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(Validation())
+            {
+                if(shapeSelection == "Ellipse")
+                {
+                    Ellipse ellipse = new Ellipse();
+
+                    ellipse.Width = Double.Parse(WidthField.Text);
+                    ellipse.Height = Double.Parse(HeightField.Text);
+
+                    BrushConverter conv = new BrushConverter();
+
+                    string fillColorString = FillColorField.SelectedItem.ToString().Split(' ')[1];
+                    SolidColorBrush fillColor = (SolidColorBrush)conv.ConvertFromString(fillColorString);
+                    ellipse.Fill = fillColor;
+
+                    string borderColorString = BorderColorField.SelectedItem.ToString().Split(' ')[1];
+                    SolidColorBrush borderColor = (SolidColorBrush)conv.ConvertFromString(borderColorString);
+                    ellipse.Stroke = borderColor;
+
+                    ellipse.StrokeThickness = Double.Parse(BorderThicknessField.Text);
+
+                    Canvas.SetLeft(ellipse, canvasLocation.X);
+                    Canvas.SetTop(ellipse, canvasLocation.Y);
+
+                    paintingCanvas.Children.Add(ellipse);
+                }
+
+                if(shapeSelection == "Rectangle")
+                {
+                    Rectangle rectangle = new Rectangle();
+
+                    rectangle.Width = Double.Parse(WidthField.Text);
+                    rectangle.Height = Double.Parse(HeightField.Text);
+
+                    BrushConverter conv = new BrushConverter();
+
+                    string fillColorString = FillColorField.SelectedItem.ToString().Split(' ')[1];
+                    SolidColorBrush fillColor = (SolidColorBrush)conv.ConvertFromString(fillColorString);
+                    rectangle.Fill = fillColor;
+
+                    string borderColorString = BorderColorField.SelectedItem.ToString().Split(' ')[1];
+                    SolidColorBrush borderColor = (SolidColorBrush)conv.ConvertFromString(borderColorString);
+                    rectangle.Stroke = borderColor;
+
+                    rectangle.StrokeThickness = Double.Parse(BorderThicknessField.Text);
+
+                    Canvas.SetLeft(rectangle, canvasLocation.X);
+                    Canvas.SetTop(rectangle, canvasLocation.Y);
+
+                    paintingCanvas.Children.Add(rectangle);
+                }
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Error some fields are empty!");
+                return;
+            }
+        }
+
+        private bool Validation()
+        {
+            if(WidthField.Text == string.Empty)
+            {
+                WidthField.BorderBrush = new SolidColorBrush(Colors.Red);
+                return false;
+            }    
+            if(HeightField.Text == string.Empty)
+            {
+                HeightField.BorderBrush = new SolidColorBrush(Colors.Red);
+                return false;
+            }
+            if(BorderThicknessField.Text == string.Empty)
+            {
+                BorderThicknessField.BorderBrush = new SolidColorBrush(Colors.Red);
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
